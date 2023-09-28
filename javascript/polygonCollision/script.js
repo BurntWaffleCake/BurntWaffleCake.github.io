c8685b41c92e2ec6bcce69095c7582eac7f02bf6
BurntWaffleCake <neondenetro@gmail.com>
2023-09-28 15:38:07 -0500

    update

--- javascript/polygonCollision/script.js
+++ javascript/polygonCollision/script.js
@@ -128,8 +128,8 @@ class Box {
     toWorldSpace(x, y) {
         return {
             x: this.x + x * Math.cos(this.r) - y * Math.sin(this.r),
-            y: this.y +  x * Math.sin(this.r) + y * Math.cos(this.r),
-            }
+            y: this.y + x * Math.sin(this.r) + y * Math.cos(this.r),
+        }
     }
 
     getWorldCoordinates() {
@@ -137,7 +137,7 @@ class Box {
         let hh = this.h / 2
 
         let tl = this.toWorldSpace(-hw, -hh)// { x: -hw, y: -hh }
-          let tr = this.toWorldSpace(hw, -hh)
+        let tr = this.toWorldSpace(hw, -hh)
         let bl = this.toWorldSpace(-hw, hh)
         let br = this.toWorldSpace(hw, hh)
 
@@ -152,48 +152,73 @@ class Box {
 
         ctx.strokeStyle = this.color
         ctx.beginPath();
-        ctx.moveTo(this.x, this.y)
-        ctx.lineTo(this.x + this.w / 2 * Math.cos(this.rotation), this.y + this.h / 2 * Math.sin(this.rotation))
-
         ctx.moveTo(coords[0].x, coords[0].y)
         for (let i = 0; i < coords.length; i++) {
             ctx.lineTo(coords[i].x, coords[i].y)
         }
         ctx.lineTo(coords[0].x, coords[0].y)
         ctx.stroke()
+
+        ctx.beginPath()
+        ctx.moveTo(this.x, this.y)
+        let foward = this.toWorldSpace(this.w / 2, 0)
+        ctx.lineTo(foward.x, foward.y)
+        ctx.stroke()
+
     }
 }
 
 function getCollidingPoint(box1, box2, normal) {
-    let mag = Math.sqrt((box2.x - box1.y) ** 2 + (box2.x - box1.y) ** 2)
-    let delta = { x: (box2.x - box1.y), y: (box2.x - box1.y) }
+    let mag = Math.sqrt((box2.x - box1.x) ** 2 + (box2.y - box1.y) ** 2)
+    let delta = { x: (box2.x - box1.x) / mag, y: (box2.y - box1.y) / mag }
 
-    let box1Coords = box1.getWorldCoordinates()
-    let box2Coords = box2.getWorldCoordinates()
-    let minPoint = box1.toWorldSpace(box1Coords[0].x - box1.x, box1Coords[0].y - box1.y)
-    let minDot = Math.abs(delta.x * minPoint.x + minPoint.y * delta.y)
+    ctx.beginPath()
+    ctx.moveTo(box1.x, box1.y)
+    ctx.lineTo(box1.x + delta.x / 2 * mag, box1.y + delta.y / 2 * mag)
+    ctx.stroke()
+
+    let box1Coords = box1.getCoordinates()
+    let box2Coords = box2.getCoordinates()
+
+    let maxPoint = box1.toWorldSpace(box1Coords[0].x, box1Coords[0].y)
+    let maxPointMag = Math.sqrt(maxPoint.x * maxPoint.x + maxPoint.y * maxPoint.y)
+    let maxDot = delta.x * (box1Coords[0].x / maxPointMag) + delta.y * (box1Coords[0].y / maxPointMag)
+
+    ctx.fillText(String(maxDot), maxPoint.x, maxPoint.y)
+
+    console.log(maxDot)
 
     for (let i = 1; i < box1Coords.length; i++) {
         let point = box1Coords[i]
-        point.x -= box1.x
-        point.y -= box1.y
-        let dot = delta.x * point.x + point.y * delta.y
-        if (dot < minDot) {
-            minDot = dot
-            minPoint = {x: box1.x + point.x, y: box1.y + point.y}
+        let worldPoint = box1.toWorldSpace(point.x, point.y)
+        let pointMag = Math.sqrt(point.x*point.x + point.y*point.y)
+        let norm = {x: point.x/pointMag, y: point.y/pointMag}
+
+        let dot = delta.x*norm.x + delta.y*norm.y
+
+        if (dot > maxDot) {
+            maxDot = dot
+            maxPoint = worldPoint
         }
+
+        ctx.fillText(String(dot), worldPoint.x, worldPoint.y)
     }
 
     for (point of box2Coords) {
-        point.x -= box2.x
-        point.y -= box2.y
-        let dot = delta.x * point.x + point.y * delta.y
-        if (dot < minDot) {
-            minDot = dot
-            minPoint =  {x: box2.x + point.x, y: box2.y + point.y}
+        let worldPoint = box2.toWorldSpace(point.x, point.y)
+        let pointMag = Math.sqrt(point.x*point.x + point.y*point.y)
+        let norm = {x: point.x/pointMag, y: point.y/pointMag}
+
+        let dot = -delta.x*norm.x + -delta.y*norm.y
+
+        if (dot < maxDot) {
+            maxDot = dot
+            maxPoint = worldPoint
         }
+
+        ctx.fillText(String(dot), worldPoint.x, worldPoint.y)
     }
-    return minPoint
+    return maxPoint
 }
 
 function testProjectionOverlap(proj1, proj2) {
@@ -243,7 +268,6 @@ function testBoxCollision(box1, box2) {
     satAxis = satAxis.concat(box2.getCollisionAxis())
 
     var colliding = true
-
     let mvt = Number.MAX_VALUE
     let normal = undefined
 
@@ -374,55 +398,55 @@ function loop(t) {
 
     clearCanvas()
     if (mouse1Down) {
-        for (box of boxes) {
-            let point = getMousePos(canvas, mousePos)
-            let delta = { x: point.x - box.x, y: point.y - box.y }
-            box.dx += delta.x* 10 * dt
-            box.dy += delta.y* 10 * dt
-        }
-     
-        // let point = getMousePos(canvas, mousePos)
-        // let closestBox = boxes[0]
-        // let distance = Math.sqrt((point.x - closestBox.x) ** 2 + (point.y - closestBox.y) ** 2)
-        // for (let i = 1; i < boxes.length; i++) {
-        //     let nextBox = boxes[i]
-        //     let nextDistance = Math.sqrt((point.x - nextBox.x) ** 2 + (point.y - nextBox.y) ** 2)
-        //     if (nextDistance < distance) {
-        //         closestBox = nextBox
-        //         distance = nextDistance
-        //     }
+        // for (box of boxes) {
+        //     let point = getMousePos(canvas, mousePos)
+        //     let delta = { x: point.x - box.x, y: point.y - box.y }
+        //     box.dx += delta.x* 10 * dt
+        //     box.dy += delta.y* 10 * dt
         // }
-        // closestBox.x = point.x
-        // closestBox.y = point.y
 
-        // closestBox.dx = mouseDelta.x * 50
-        // closestBox.dy = mouseDelta.y * 50
+        let point = getMousePos(canvas, mousePos)
+        let closestBox = boxes[0]
+        let distance = Math.sqrt((point.x - closestBox.x) ** 2 + (point.y - closestBox.y) ** 2)
+        for (let i = 1; i < boxes.length; i++) {
+            let nextBox = boxes[i]
+            let nextDistance = Math.sqrt((point.x - nextBox.x) ** 2 + (point.y - nextBox.y) ** 2)
+            if (nextDistance < distance) {
+                closestBox = nextBox
+                distance = nextDistance
+            }
+        }
+        closestBox.x = point.x
+        closestBox.y = point.y
+
+        closestBox.dx = mouseDelta.x * 50
+        closestBox.dy = mouseDelta.y * 50
     }
 
     for (let c = 0; c < 1; c++) {
         let cdt = dt / 1
-        calculatePhysics(cdt)
+        // calculatePhysics(cdt)
 
         for (let i = 0; i < boxes.length; i++) {
             let box1 = boxes[i]
             for (let n = i + 1; n < boxes.length; n++) {
                 let box2 = boxes[n]
-    
+
                 let result = testBoxCollision(box1, box2)
                 if (result.colliding) {
                     // console.log("collided")
-    
+
                     let collisionPoint = getCollidingPoint(box1, box2)
                     ctx.fillStyle = "rgb(255, 0, 0)"
-                    ctx.fillRect(collisionPoint.x - 1.5, collisionPoint.y - 1.5, 10, 10)
-                    resolveCollision(box1, box2, result.mtx, result.normal)
+                    ctx.fillRect(collisionPoint.x - 5, collisionPoint.y - 5, 10, 10)
+                    // resolveCollision(box1, box2, result.mtx, result.normal)
                 }
             }
         }
 
     }
 
-    
+
 
     for (box of boxes) {
         box.render(ctx)
@@ -435,12 +459,12 @@ function loop(t) {
 function startup() {
     console.log("Starting simulation")
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 2; i++) {
         boxes.push(new Box(
             Math.random() * width,
             Math.random() * height,
-            25 + Math.random() * 75,
-            25 + Math.random() * 75,
+            100 + Math.random() * 75,
+            100 + Math.random() * 75,
             360 * Math.random(),
             150 - Math.random() * 300,
             150 - Math.random() * 300,
