437a0e3b0a4b4cccb437d819088f8e5211229b68
BurntWaffleCake <neondenetro@gmail.com>
2023-10-21 13:19:17 -0500

    Models

--- javascript/PhysicsEngine/main.js
+++ javascript/PhysicsEngine/main.js
@@ -6,15 +6,23 @@ let ctx = src.getContext("2d");
 
 let poly1
 let poly2
+let model
 
 let bounds = []
 let polygons = []
+let models = []
 
 function clearCanvas(ctx) {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
 
 function render(dt) {
+
+    model.render(ctx)
+
+    for (let model of models) {
+        model.render(ctx)
+    }
 
     for (let polygon of polygons) {
         polygon.render(ctx)
@@ -51,11 +59,33 @@ function applyCollisionBounds(polygon) {
 function calculate(dt, t) {
     if (paused) { return }
 
+    for (let model of models) {
+        for (let i = 0; i < 4; i++) {
+            let dti = dt / 4
+            model.vel.y += 1000 * dt
+            model.tick(dti, t)
+            // if (!polygon.anchored) {
+            //     // applyCollisionBounds(polygon)
+            // }
+
+            for (let collPoly of polygons) {
+                let results = model.testPolygonCollision(collPoly, ctx)
+
+                results.forEach(result => {
+                    if (result == false) { return }
+                    model.resolvePolygonCollision(result.polygon, result.mvt, result.normal, result.points)
+                });
+            }
+        }
+        // console.log(model)
+    }
+
     for (let polygon of polygons) {
-        for (let i = 0; i < 1; i++) {
-            let dti = dt / 1
+        for (let i = 0; i < 4; i++) {
+            let dti = dt / 4
+
+            polygon.vel.y += 1000 * dti
             polygon.tick(dti, t)
-            // polygon.vel.y += 1000 * dti
 
             if (!polygon.anchored) {
                 // applyCollisionBounds(polygon)
@@ -63,13 +93,15 @@ function calculate(dt, t) {
 
             for (let collPoly of polygons) {
                 if (collPoly === polygon) { continue }
-                let result = polygon.bruteForceTestCollision(collPoly, ctx)
+                let result = polygon.testCollision(collPoly, ctx)
                 if (result == false) { continue }
-                polygon.resolveCollision(collPoly, result.mvt, result.normal, result.point)
+                polygon.resolveCollision(collPoly, result.mvt, result.normal, result.points)
             }
         }
 
     }
+
+    
 
 }
 
@@ -81,8 +113,9 @@ function loop(t) {
     let dt = (t / 1000) - time;
 
     if (mouse1down) {
-        let delta = new Vector2(prevMouseEvent.clientX, prevMouseEvent.clientY).subtract(poly1.pos)
-        poly1.applyImpulse(delta.scale(poly1.mass / 2), poly1.pos)
+        let delta = new Vector2(prevMouseEvent.clientX, prevMouseEvent.clientY).subtract(model.pos)
+        // model.pos.set(prevMouseEvent.clientX, prevMouseEvent.clientY)
+        model.applyImpulse(delta.scale(model.mTot / 2), model.pos)
     }
 
     calculate(dt, t / 1000);
@@ -101,9 +134,6 @@ let boundThickness = 1000
 function startup() {
     updateCanvasSize();
 
-    // poly1 = new polyModule.Box(new Vector2(ctx.canvas.width / 2 , ctx.canvas.height / 2), new Vector2(300, 100), 65, undefined, -0)
-    // polygons.push(poly1)
-
     let top = new polyModule.Box(new Vector2(ctx.canvas.width / 2, -boundThickness / 2), new Vector2(ctx.canvas.width + boundThickness * 2, boundThickness), 0, undefined, undefined)
     top.anchored = true
     polygons.push(top)
@@ -119,42 +149,49 @@ function startup() {
     let right = new polyModule.Box(new Vector2(ctx.canvas.width + boundThickness / 2, ctx.canvas.height / 2), new Vector2(boundThickness, ctx.canvas.height + boundThickness * 2), 0, undefined, undefined)
     right.anchored = true
     polygons.push(right)
-    // let right = 
-    // let bottom = new polyModule.Box(new Vector2(ctx.canvas.width + boundThickness * 2, boundThickness), new Vector2(ctx.canvas.width / 2, ctx.canvas.height + boundThickness), 0, undefined, undefined)
-    // bottom.anchored = true
-    // polygons.push(bottom)
-    // let left = 
 
-    poly1 = new polyModule.RegularPolygon(new Vector2(ctx.canvas.width / 2, ctx.canvas.height / 2), new Vector2(100, 100), 30, 0, undefined, -20)
 
+    model = new polyModule.Model([
+        new polyModule.Box(new Vector2(0, 0), new Vector2(100, 50), 0),
+        new polyModule.Box(new Vector2(75, 0), new Vector2(50, 150), 0),
+        new polyModule.Box(new Vector2(-75, 0), new Vector2(50, 150), 0)
+    ], new Vector2(ctx.canvas.width / 2, ctx.canvas.height / 2), 0)
+    console.log(model)
+    models.push(model)
+
+    poly1 = new polyModule.Box(new Vector2(ctx.canvas.width / 2 , ctx.canvas.height / 2), new Vector2(300, 100), 0, undefined, -0)
     polygons.push(poly1)
 
-    // poly2 = new polyModule.Box(new Vector2(ctx.canvas.width / 2, ctx.canvas.height - 100), new Vector2(3000, 100), 5, undefined, 0)
+    // poly1 = new polyModule.RegularPolygon(new Vector2(ctx.canvas.width / 2, ctx.canvas.height / 2), new Vector2(100, 100), 30, 0, undefined, -20)
+    // polygons.push(poly1)
+
+    poly2 = new polyModule.Box(new Vector2(ctx.canvas.width / 2, ctx.canvas.height - 100), new Vector2(3000, 100), 5, undefined, 0)
     // polygons.push(poly2)
-    // poly2.anchored = true
+    poly2.anchored = true
     // poly2 = new polyModule.RegularPolygon(new Vector2(ctx.canvas.width / 2, ctx.canvas.height / 2), new Vector2(100, 150), 50, 0, undefined, 10)
     // poly2 = new polyModule.Wall(new Vector2(ctx.canvas.width / 2, ctx.canvas.height / 2), 500, 0, new Vector2(0, 0), 0)
-    // polygons.push(poly2)
-    for (let i = 0; i < 25; i++) {
-        polygons.push(
-            new polyModule.RegularPolygon(
-                new Vector2(ctx.canvas.width * Math.random(), ctx.canvas.height * Math.random()),
-                new Vector2(25 + Math.random() * 100, 25 + Math.random() * 100),
-                3 + Math.floor(Math.random() * 3),
-                0,
-                undefined,
-                180 * Math.random()
-            )
+    polygons.push(poly2)
+    
+    // for (let i = 0; i < 25; i++) {
+    //     polygons.push(
+    //         new polyModule.RegularPolygon(
+    //             new Vector2(ctx.canvas.width * Math.random(), ctx.canvas.height * Math.random()),
+    //             new Vector2(25 + Math.random() * 100, 25 + Math.random() * 100),
+    //             3 + Math.floor(Math.random() * 3),
+    //             0,
+    //             undefined,
+    //             180 * Math.random()
+    //         )
 
-            // new polyModule.Box(
-            //     new Vector2(ctx.canvas.width * Math.random(), ctx.canvas.height * Math.random()),
-            //     new Vector2(25 + Math.random() * 100, 25 + Math.random() * 100),
-            //     0,
-            //     undefined,
-            //     180 * Math.random(),
-            // )
-        )
-    }
+    //         // new polyModule.Box(
+    //         //     new Vector2(ctx.canvas.width * Math.random(), ctx.canvas.height * Math.random()),
+    //         //     new Vector2(25 + Math.random() * 100, 25 + Math.random() * 100),
+    //         //     0,
+    //         //     undefined,
+    //         //     180 * Math.random(),
+    //         // )
+    //     )
+    // }
 
     window.requestAnimationFrame(loop);
 }
