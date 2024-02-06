7545360cca96ba44cf2b80fd57308d02559bb360
BurntWaffleCake <neondenetro@gmail.com>
2024-02-05 23:06:31 -0600

    started article framwork

--- public/js/index.js
+++ public/js/index.js
@@ -6,13 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   tl.add({
     targets: "#vanta-globe",
-    translateX: 0,
+    translateX: "-50px",
+
     easing: "spring(1, 80, 10, 0)",
+    complete: connectMouseMove,
   })
     .add(
       {
         targets: "#main-background",
         translateX: 0,
+
         opacity: 1,
       },
       "-=1000"
@@ -43,3 +46,31 @@ document.addEventListener("DOMContentLoaded", () => {
       "-=300"
     );
 });
+
+const links = document.getElementsByClassName("link");
+for (let link of links) {
+  link.onmouseover = () => {
+    anime({
+      targets: link,
+      translateX: 15,
+    });
+  };
+
+  link.onmouseout = () => {
+    anime({
+      targets: link,
+      translateX: 0,
+    });
+  };
+}
+
+const offset = 250;
+const background = document.getElementById("vanta-globe");
+function connectMouseMove() {
+  // document.onmousemove = function (evt) {
+  //   let xRatio = evt.clientX / window.innerWidth - 0.5;
+  //   let yRatio = evt.clientY / window.innerHeight - 0.5;
+  //   background.style.top = `${window.innerHeight / 2 + offset * yRatio}px`;
+  //   background.style.left = `${window.innerWidth / 2 + offset * xRatio}px`;
+  // };
+}
