e7568c7992c1ef3329726406fc577fcb1f47cda5
BurntWaffleCake <neondenetro@gmail.com>
2024-02-08 10:16:53 -0600

    added environment file for static pages

---
+++ public/environment/mainPages.js
@@ -0,0 +1,28 @@
+const env_main_pages = {
+  mainPageLinks: [
+    {
+      type: "basic", //basic, dropdown
+      name: "Home",
+      href: "/public/index.html",
+    },
+    {
+      type: "dropdown", //basic, dropdown
+      name: "Home",
+      href: "/public/index.html",
+      items: [
+        {
+          name: "brpther",
+          href: "/public/index.html",
+        },
+        {
+          name: "in",
+          href: "/public/index.html",
+        },
+        {
+          name: "christ",
+          href: "/public/index.html",
+        },
+      ],
+    },
+  ],
+};
--- public/js/article.js
+++ public/js/article.js
@@ -1,3 +1,32 @@
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) => (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16));
+}
+
+const mainNavBar = document.getElementById("mainNavBar");
+env_main_pages.mainPageLinks.forEach((navItem) => {
+  if (navItem.type == "basic") {
+    mainNavBar.innerHTML += `
+    <li class="nav-item">
+      <a class="nav-link" href="${navItem.href}">${navItem.name}</a>
+    </li>
+  `;
+  } else if (navItem.type == "dropdown") {
+    let inner = "";
+    navItem.items.forEach((item) => {
+      inner += `<a class="dropdown-item" href="${item.href}">${item.name}</a>`;
+    });
+
+    let id = uuidv4();
+    mainNavBar.innerHTML += `<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="${id}" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">${navItem.name}</a>
+        <div class="dropdown-menu mb-3" aria-labelledby="${id}">
+          ${inner}
+        </div>
+      </li>
+    `;
+  }
+});
+
 const html = document.getElementById("html");
 const button = document.getElementById("theme_toggle_button");
 
